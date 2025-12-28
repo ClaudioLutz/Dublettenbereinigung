5277cb68cea9d477609f7c932eab81816168fdc0
Claudio Lutz <claudio.lutz@creditreform.ch>
2025-12-28 22:49:49 +0100

    Remove top limit from SQL query and eliminate unnecessary filter on PLZ

--- query.sql
+++ query.sql
@@ -1,4 +1,4 @@
-SELECT top 100000
+SELECT 
        [Name]
       ,[Vorname]
       ,[Name2]
@@ -12,5 +12,4 @@ SELECT top 100000
       ,[Erfasst]
       ,[Quelle_95]
   FROM [CAG_Analyse].[dbo].[vAdresse_Quelle95]
-  where Plz like '8%'
   ORDER BY PLZ,Strasse desc
